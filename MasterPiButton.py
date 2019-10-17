c6a6d4c222c5fca9ca2781a8638af6f57fb3b49b
jman19 <jasonguyw@gmail.com>
2019-10-17 17:20:34 -0400

    updated to call masterPi api

--- MasterPiButton.py
+++ MasterPiButton.py
@@ -1,9 +1,12 @@
 import RPi.GPIO as GPIO #Use GPIO library
 import time #Use time library
+import requests #requests library
 
 PrevButtonPin = 15  
 ResetButtonPin = 13
 NextButtonPin = 11
+
+URL="http://127.0.0.1:8080/"
 
 GPIO.setmode(GPIO.BOARD)  # Numbers GPIOs by physical location
 
@@ -14,12 +17,24 @@ GPIO.setup(NextButtonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)    #Set NextButtonP
 
 def nextPressed(channel):
     print('Next Pressed')
+    try:
+    	r=requests.put(url=URL+"next")
+    except:
+    	print('failed to update index')
     
 def prevPressed(channel):
     print('Prev Pressed') #Display button pressed
+    try:
+    	r=requests.put(url=URL+"prev")
+    except:
+    	print('failed to update index')
     
 def resetPressed(channel):
     print('Reset Pressed')
+    try:
+    	r=requests.put(url=URL+"reset")
+    except:
+    	print('failed to update index')
     
 GPIO.add_event_detect(PrevButtonPin,GPIO.FALLING,callback=prevPressed,bouncetime=200)
 GPIO.add_event_detect(ResetButtonPin,GPIO.FALLING,callback=resetPressed,bouncetime=200)
